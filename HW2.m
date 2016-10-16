da35b09dcedaee176c0b5ec2503c12c5f11a969a
mtomic <mihajlo@gmail.com>
2016-10-16 11:31:54 -0700

    Add Steepest Descent for intial trial step length
    The alpha_0 direction varies depending on method.
    For Newton//quasi-Newton alpha_0 is 1; however for
    steepest descent and conjugate gradient there are
    a couple of strategies tio employ - one of which is
    included here.

--- HW2.m
+++ HW2.m
@@ -3,34 +3,46 @@ clear
 TOLERANCE = 10^-8;
 MAXITER = 20e3;
 alpha_val = 1; % alpha_0, alpha_max
-x=[-1.2;1.2];
+x=[1.2;1.2];
 c1=10^(-4);
 c2= 9/10;
 
-Grad_Rosenbrock(x(1),x(2));
+use_newton = 1;
 
 k = 0;
-fprintf('x^T \t\t\t\t\tRosenbrock \t\t\t p^ \t\t\t\t\talpha \t\titer\n');
+fprintf('x^T \t\t\t\t\tRosenbrock \t\t\t p_k \t\t\t\t\talpha \t\titer\n');
 
 while abs(Rosenbrock(x(1),x(2)))  > TOLERANCE 
     % Search direction
-    %p_k = P_k(x(1),x(2));
-    p_k = P_k_newton(x(1), x(2));
-
+    % Newton method or Steepest Descent 
+    if use_newton
+        p_k = P_k_newton(x(1), x(2));
+    else
+        p_k = P_k(x(1), x(2));
+    end
+        
     alpha_val = Linesearch(alpha_val, x, c1, c2, p_k);
     
     % Update the minimizer
-    x = x + alpha_val*P_k_newton(x(1),x(2));
+    x_minus_1 = x; % Use to update the alpha for steepest descent
+    x = x + alpha_val*p_k;
 
     k=k+1;
-    if mod(k,1) == 0
+    if mod(k,1000) == 0
         fprintf('[%1.6f,%1.6f]\t\t[%1.6f]\t\t[%1.6f,%1.6f]\t\t%1.6f\t%1.6f\n',...
-            x(1),x(2),Rosenbrock(x(1),x(2)), P_k_newton(x(1),x(2)) , alpha_val, k);
+            x(1),x(2),Rosenbrock(x(1),x(2)), p_k , alpha_val, k);
     end
-    alpha_val = 1; % For Newton method
+    % Update alpha based on the search direction
+    if use_newton
+        alpha_val = 1; % For Newton method
+    else
+        alpha_val = alpha_val*(p_k'*Grad_Rosenbrock(x_minus_1(1),x_minus_1(2))) / ...
+                          (P_k(x(1), x(2))' * Grad_Rosenbrock(x(1),x(2)));
+    end
+        
     if k > MAXITER
         error('Maximum iteration met - abort search', MAXITER)
     end
 end
 fprintf('[%1.6f,%1.6f]\t\t[%1.6f]\t\t[%1.6f,%1.6f]\t\t\t%1.6f\t%1.6f\n',...
-       x(1),x(2),Rosenbrock(x(1),x(2)), P_k_newton(x(1),x(2)) , alpha_val, k);
+       x(1),x(2),Rosenbrock(x(1),x(2)), p_k, alpha_val, k);
